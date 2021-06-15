1574ac4caee18011fdee54f24e91a14734956cd1
Nicholas Jimenez <nicholasd.jimenez@gmail.com>
2021-06-15 14:16:53 -0700

    Reviewws view commit

--- movie/views.py
+++ movie/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from movie.models import Review
+from django.shortcuts import render, get_object_or_404
+from .models import Film, MovieType, Review
+from django.urls import reverse_lazy
+
+
 
 # Create your views here.
 def index(request):
     return render(request, 'movie/index.html')
+
+def reviews(request):
+    review_list=Review.objects.all()
+    return render(request, 'movie/reviews.html', {'review_list': review_list})
+
